f704d3036940f3bab37216f326f5007dceb58446
abhiparkar45 <abhishekparkar48@gmail.com>
2022-11-15 10:05:24 +0530

    v2.0.0

--- async-await.js
+++ async-await.js
@@ -1,38 +1,34 @@
-const authenticate = async(username,password) => {
-    console.log(`authenticating with userId : ${username} and ${password}...`);
-    return new Promise((resolve,reject) => {
-        setTimeout(()=>{
-            if(username === 'user123' && password ==='password'){
-                resolve( {success:true} );
-            }
-            reject (new Error("wrong username or password !"));
-        },3000)
-    })
-    
-    
-}
-const fetchData = async() =>{
-    console.log(`getting data! please wait...`);
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve( {id:1, name:'user'} );
-    },4000)
-    })
-    
-    
-}
+const authenticate = async (username, password) => {
+  console.log(`authenticating with userId : ${username} and ${password}...`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (username === "user123" && password === "password") {
+        resolve({ success: true });
+      }
+      reject(new Error("wrong username or password !"));
+    }, 3000);
+  });
+};
+const fetchData = async () => {
+  console.log(`getting data! please wait...`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ id: 1, name: "user" });
+    }, 4000);
+  });
+};
 
-const getData = async() =>{
-    try{
-    const res = await authenticate('user123','password');
-    if(res.success){
-        const data = await fetchData();
-        console.log(data);
+const getData = async () => {
+  try {
+    const res = await authenticate("user123", "password");
+    console.log(res);
+    if (res.success) {
+      const data = await fetchData();
+      console.log(data);
     }
-}
-catch(err){
-    console.log(err.message)
-}
-}
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 getData();
